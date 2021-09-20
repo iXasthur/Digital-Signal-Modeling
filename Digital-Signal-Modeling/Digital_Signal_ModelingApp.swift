777149426f1d01e63a8a5e0a16bab454e0d519d1
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-09-20 14:01:41 +0300

    Initial Commit

---
+++ Digital-Signal-Modeling/Digital_Signal_ModelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_Signal_ModelingApp.swift
+//  Digital-Signal-Modeling
+//
+//  Created by Михаил Ковалевский on 20.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Digital_Signal_ModelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
