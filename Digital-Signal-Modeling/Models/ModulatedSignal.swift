ebb0b21ae357e89a7e653779b3514649af015a7b
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-09-25 14:41:56 +0300

    Update ModulatedSignal.swift

--- Digital-Signal-Modeling/Models/ModulatedSignal.swift
+++ Digital-Signal-Modeling/Models/ModulatedSignal.swift
@@ -19,25 +19,74 @@ struct ModulatedSignal: Signal {
     
     var type: ModulationType = .frequency
     
-    var message: HarmonicSignal = HarmonicSignal.createSine(amplitude: 1, startPhase: 0, frequency: 3)
-    var carrier: HarmonicSignal = HarmonicSignal.createSine(amplitude: 1, startPhase: 0, frequency: 18)
+    var message: HarmonicSignal = HarmonicSignal.createSine(amplitude: 1, startPhase: 0, frequency: 4)
+    var carrier: HarmonicSignal = HarmonicSignal.createSine(amplitude: 1, startPhase: 0, frequency: 15)
     
     private func getValuesFM(_ count: Int) -> [Double] {
-        let messageValues: [Double] = message.getValues(count)
-        
         var values: [Double] = []
-        for n in 0..<count {
-            let t = Double(n) / Double(count)
-            
-            var s = carrier
-            s.frequency = s.frequency! + (s.frequency! / 8.0 * messageValues[n] / message.amplitude!)
-            let phase = 2.0 * Double.pi * s.frequency! * t
-            let y = s.getValue(phase: phase)
-//            print("\(n): msg:\(messageValues[n]) frequency:\(s.frequency!) phase:\(phase) y:\(y)")
-            
-            values.append(y)
+        
+        let mf = message.frequency ?? 1
+        let cf = carrier.frequency ?? 1
+
+        var messagePhase: Double = message.startPhase ?? 0
+        var carrierPhase: Double = carrier.startPhase ?? 0
+
+        for _ in 0..<count {
+            values.append(carrier.getValue(phase: carrierPhase))
+
+            messagePhase += 2 * Double.pi * mf / Double(count)
+
+            carrierPhase += 2 * Double.pi * cf / Double(count) * (2 + self.message.getValue(phase: messagePhase) / message.amplitude!)
+
+
+            if carrierPhase >= 2 * Double.pi {
+                carrierPhase -= 2 * Double.pi
+            }
+            if carrierPhase < 0.0 {
+                carrierPhase += 2 * Double.pi
+            }
         }
+
         return values
+        
+        
+//        let messageValues: [Double] = message.getValues(count)
+//
+//        var values: [Double] = []
+//        for n in 0..<count {
+//            let t = Double(n) / Double(count)
+//
+//            var s = carrier
+//            s.frequency = s.frequency! + (s.frequency! / 8.0 * messageValues[n] / message.amplitude!)
+//            let phase = 2.0 * Double.pi * s.frequency! * t + s.startPhase!
+//            let y = s.getValue(phase: phase)
+//            print("\(n): msg:\(messageValues[n]) frequency:\(s.frequency!) phase:\(phase) y:\(y)")
+//
+//            values.append(y)
+//        }
+//        return values
+        
+//        let messageValues: [Double] = message.getValues(count)
+//
+//        var values: [Double] = []
+//        for n in 0..<count {
+//            let t = Double(n) / Double(count)
+//
+//            let simpson = { () -> Double in
+//                let a0 = t / 8
+//                let s0 = messageValues[0]
+//                let s1 = 3 * messageValues[Int((Double(n)/3).rounded())]
+//                let s2 = messageValues[Int((2*Double(n)/3).rounded())]
+//                let s3 = messageValues[n]
+//                return a0 * (s0 + s1 + s2 + s3)
+//            }
+//
+//            let phase = 2.0 * Double.pi * carrier.frequency! * t + 2.0 * Double.pi * 5 * simpson()
+//            let y = carrier.getValue(phase: phase)
+//
+//            values.append(y)
+//        }
+//        return values
     }
     
     private func getValuesAM(_ count: Int) -> [Double] {
